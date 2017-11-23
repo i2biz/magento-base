b65e663557be7d751e02c8db16f9cc4847046c58
Jacek Bzdak <jbzdak@gmail.com>
2017-11-23 14:18:24 +0100

    Added jpeg to gd.

--- setup-system.sh
+++ setup-system.sh
@@ -10,6 +10,8 @@ apt-get update -q
 apt-get upgrade -qy
 apt-get install -qy unzip libgd-dev libicu-dev libmcrypt-dev libmhash-dev libssl-dev libxml2-dev libxslt-dev libjpeg-dev libpng-dev
 
+docker-php-ext-configure gd --with-jpeg-dir=/usr/lib
+
 docker-php-ext-install gd intl mcrypt openssl pdo_mysql soap xml xsl zip json iconv opcache
 
 rm -rf /var/lib/apt/lists/
