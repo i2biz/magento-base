dfb07d260065d005a4b161eff50ec8e8312ca959
Jacek Bzdak <jbzdak@gmail.com>
2017-11-23 14:59:14 +0100

    Another one.

--- setup-system.sh
+++ setup-system.sh
@@ -8,9 +8,10 @@ adduser magento magento
 
 apt-get update -q
 apt-get upgrade -qy
-apt-get install -qy unzip libgd-dev libicu-dev libmcrypt-dev libmhash-dev libssl-dev libxml2-dev libxslt-dev libjpeg-dev libpng-dev
+apt-get install -qy unzip libgd-dev libicu-dev libmcrypt-dev libmhash-dev libssl-dev libxml2-dev libxslt-dev \
+        libjpeg-dev libpng-dev libfreetype6-dev
 
-docker-php-ext-configure gd --with-jpeg-dir=/usr/lib
+docker-php-ext-configure gd --with-jpeg-dir=/usr/lib --with-freetype-dir=/usr/lib
 
 docker-php-ext-install gd intl mcrypt openssl pdo_mysql soap xml xsl zip json iconv opcache
 
